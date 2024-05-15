91490f7a94d7bd89aaa8100db7c9743ac5437135
nguyễn mạnh <manharia123@gmail.com>
2024-05-16 00:05:36 +0700

    Singleton type 1

---
+++ Singleton/singletonType1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+    public class MySingletonType1 : MonoBehaviour
+    {
+    // tạo 1 biến chung của lớp MySingletonType1 , private set để hạn chế truy cập từ bên ngoài 
+    public static MySingletonType1 instance { get; private set; }
+ 
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void DoSomething()
+    {
+        Debug.Log("Doing something...");
+    }
+}
+
