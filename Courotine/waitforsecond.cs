05f65c53e65fa5f16f0f464c15744bb939add5d8
nguyễn mạnh <manharia123@gmail.com>
2024-05-17 00:16:45 +0700

    courotine = waitforseconds

---
+++ Courotine/waitforsecond.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waitforsecond : MonoBehaviour
+{
+    
+    void Start()
+    {
+        StartCoroutine(DoSomething());
+    }
+    private IEnumerator DoSomething()
+    {
+        Debug.Log("Bắt đầu Coroutine");
+
+        // Đợi 2 giây
+        yield return new WaitForSeconds(2f);
+
+        Debug.Log("Đợi 2 giây!");
+
+        // Thực hiện hành động khác
+        Debug.Log("Tiếp tục thực hiện hành động sau khi đợi xong");
+    }
+
+}
