ff535774197a6108e0a94fbb7442336c2a3e52aa
nguyễn mạnh <manharia123@gmail.com>
2024-05-17 18:00:41 +0700

    DrawGizmo - vec 1 gizmo

---
+++ Gizmo/drawgizmo.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class MyGizmoDrawer : MonoBehaviour
+{
+    [SerializeField] Vector2Int m_Position;
+    [DrawGizmo(GizmoType.Selected | GizmoType.Active)]
+    private void OnDrawGizmos()
+    {
+        // Vẽ Gizmo chỉ khi đối tượng được chọn hoặc đang hoạt động
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, new Vector3(m_Position.x,m_Position.y,0f));
+    }
+}
