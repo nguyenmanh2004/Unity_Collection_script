d10f481400f9982383500a6f1871b5d875e1732d
manh <procnttdzaivl@gmail.com>
2024-07-06 04:41:32 +0700

    count-down

---
+++ Time/Count_Down_Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class count_down_timer : MonoBehaviour
+{
+    public float countdownTimer ; // Biến public để countDown
+
+    void Update()
+    {
+        // Giảm countdownTimer theo thời gian deltaTime
+        countdownTimer -= Time.deltaTime;
+
+        // Kiểm tra nếu countdownTimer đã về 0 hoặc thấp hơn
+        if (countdownTimer <= 0)
+        {
+            // Thực hiện hành động khi countdownTimer về 0
+            Debug.Log("Countdown finished!");
+        }
+    }
+}
