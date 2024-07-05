985c8404cad5299f6d7e883c2423433f5353e84b
manh <procnttdzaivl@gmail.com>
2024-07-06 04:34:03 +0700

    open-webstie

---
+++ Web/Open_Website.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Open_Website : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI textWeb;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+    public void OpenWebPage()
+    {
+        // Mở liên kết web khi Text được nhấn
+        Application.OpenURL(textWeb.text);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
