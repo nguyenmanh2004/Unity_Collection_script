9bd634eee7c1eafc9f2b32d0258f01c22db13e60
manh <procnttdzaivl@gmail.com>
2024-08-03 17:21:08 +0700

    ISerializationCallbackReceiver

---
+++ Interface/_ISerializationCallbackReceiver.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+
+public class MyCustomObject : MonoBehaviour, ISerializationCallbackReceiver
+{
+    public int someValue = 42;
+    private int internalValue;
+
+    private void Start()
+    {
+        Debug.Log("someValue: " + someValue);
+        Debug.Log("internalValue: " + internalValue);
+    }
+
+    public void OnBeforeSerialize()
+    {
+        // Chuẩn bị dữ liệu trước khi object được lưu
+        internalValue = someValue * 2;
+    }
+
+    public void OnAfterDeserialize()
+    {
+        // Khôi phục lại trạng thái của object sau khi nó được tải
+        internalValue = someValue * 2;
+        Debug.Log("OnAfterDeserialize - someValue: " + someValue + ", internalValue: " + internalValue);
+    }
+}
