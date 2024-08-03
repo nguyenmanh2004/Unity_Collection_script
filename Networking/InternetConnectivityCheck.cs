c2429c811a2136189fca1ffddd3839b8b22a491e
manh <procnttdzaivl@gmail.com>
2024-08-03 17:45:35 +0700

    InternetConnectivityCheck

---
+++ Networking/InternetConnectivityCheck.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InternetConnectivityCheck : MonoBehaviour
+{
+    void Start()
+    {
+        CheckInternetConnection();
+    }
+
+    void CheckInternetConnection()
+    {
+        if (Application.internetReachability != NetworkReachability.NotReachable)
+        {
+            Debug.Log("Kết nối Internet thành công!");
+        }
+        else
+        {
+            Debug.Log("Kết nối Internet thất bại!");
+        }
+    }
+}
