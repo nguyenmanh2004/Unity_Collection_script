b92f223cca480234ea24c3bf4a07b5bba3929e56
manh <manharia123@gmail.com>
2024-08-16 07:58:59 +0700

    Time_Line

---
+++ TimeLine/TimelineController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+
+public class TimelineController : MonoBehaviour
+{
+    private PlayableDirector playableDirector;
+   
+  
+
+    private void Start()
+    {
+        playableDirector = GetComponent<PlayableDirector>();
+        playableDirector.stopped += OnTimelineFinished;
+
+    }
+
+    private void OnTimelineFinished(PlayableDirector obj)
+    {
+        // Thực hiện logic sau khi timeline kết thúc
+        RunScriptAfterTimeline();
+    }
+
+    private void RunScriptAfterTimeline() => SceneManager.LoadScene(1);
+}
