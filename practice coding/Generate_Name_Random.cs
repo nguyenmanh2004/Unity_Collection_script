ff47613e2110977b85692d0209524cf99c7eb9cf
manh <manharia123@gmail.com>
2024-09-15 00:13:24 +0700

    Generate_Name_Random

---
+++ practice coding/Generate_Name_Random.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Generate_Name : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI textname;
+    public int typenameFemmale = 5;
+    public int typenameMale = 6;
+    public string curentName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textname.text = "name";
+    }
+    public void SetnameFemale()
+    {
+        do
+        {
+            textname.text = string.Empty; // Đặt lại textname.text thành rỗng
+            string curentName = NVJOBNameGen.Uppercase(NVJOBNameGen.GiveAName(typenameFemmale));
+            textname.text = curentName; // Gán tên mới
+        } while (string.IsNullOrEmpty(textname.text));
+    }
+    public void SetnameMale()
+    {
+        do
+        {
+            textname.text = string.Empty; // Đặt lại textname.text thành rỗng
+            string curentName = NVJOBNameGen.Uppercase(NVJOBNameGen.GiveAName(typenameMale));
+            textname.text = curentName; // Gán tên mới
+        } while (string.IsNullOrEmpty(textname.text));
+    }
+}
